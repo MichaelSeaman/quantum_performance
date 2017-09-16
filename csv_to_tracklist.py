ab6d3c37a9fa121407abf7703b761046762599d8
Michael <micseam@yahoo.com>
2017-09-15 21:45:19 -0700

    Created rewrite_csv.py

--- csv_to_tracklist.py
+++ csv_to_tracklist.py
@@ -3,66 +3,57 @@
 # Code made possible by shoogle's midicsv-process
 # https://github.com/shoogle/midicsv-process
 
-# usage:
-# python csv_to_tracklist.py <IN_CSV_FILE>
 
 import sys
 from midi import *
 
-if len(sys.argv) <= 1:
-    print("Input file needed.")
-    sys.exit(0)
-else:
-    infile = sys.argv[1]
+def csv_to_tracklist(rows):
+    tempoMap   = TempoMap()
+    notes      = []
+    noteEvents = []
+    onTicks    = []
 
-rows = open(infile, encoding="latin-1").read().splitlines()
+    # Reading midi events
+    for i, row in enumerate(rows):
+        cells = row.split(", ")
+        track = int(cells[0])
+        tick = int(cells[1])
+        rowType = cells[2]
+        if rowType == "Header":
+            tpqn = int(cells[5]) # set tickcells per quarter note
+            tempoMap.tpqn = tpqn
+            nTracks = int(cells[4])
+        elif rowType == "Tempo":
+            tempo = int(cells[3])
+            tempoMap.addTempo(tick, tempo)
+        elif rowType == "Note_on_c":
+            pitch    = int(cells[4])
+            velocity = int(cells[5])
+            noteEvents.append(NoteEvent(track, tick, pitch, velocity, i))
 
-tempoMap   = TempoMap()
-notes      = []
-noteEvents = []
-onTicks    = []
-
-# Reading midi events
-for i, row in enumerate(rows):
-    cells = row.split(", ")
-    track = int(cells[0])
-    tick = int(cells[1])
-    rowType = cells[2]
-    if rowType == "Header":
-        tpqn = int(cells[5]) # set tickcells per quarter note
-        tempoMap.tpqn = tpqn
-        nTracks = int(cells[4])
-    elif rowType == "Tempo":
-        tempo = int(cells[3])
-        tempoMap.addTempo(tick, tempo)
-    elif rowType == "Note_on_c":
-        pitch    = int(cells[4])
-        velocity = int(cells[5])
-        noteEvents.append(NoteEvent(track, tick, pitch, velocity, i))
-
-# Create notes by pairing noteOn and NoteOff events
-for i, noteEvent_on in enumerate(noteEvents):
-    if noteEvent_on.velocity ==0:
-        continue
-    for noteEvent_off in noteEvents[i:]:
-        if (noteEvent_off.velocity != 0 or
-        noteEvent_off.track != noteEvent_on.track or
-        noteEvent_off.pitch != noteEvent_on.pitch):
+    # Create notes by pairing noteOn and NoteOff events
+    for i, noteEvent_on in enumerate(noteEvents):
+        if noteEvent_on.velocity ==0:
             continue
-        note = Note(noteEvent_on, noteEvent_off)
-        notes.append(note)
-        onTicks.append(note.tick)
-        break
+        for noteEvent_off in noteEvents[i:]:
+            if (noteEvent_off.velocity != 0 or
+            noteEvent_off.track != noteEvent_on.track or
+            noteEvent_off.pitch != noteEvent_on.pitch):
+                continue
+            note = Note(noteEvent_on, noteEvent_off)
+            notes.append(note)
+            onTicks.append(note.tick)
+            break
 
-# sort notes by onTick
-notes = [x for (y,x) in sorted(zip(onTicks, notes))]
-onTicks.sort()
-tracklist = [ [] for _ in range(nTracks)]
+    # sort notes by onTick
+    notes = [x for (y,x) in sorted(zip(onTicks, notes))]
+    onTicks.sort()
+    tracklist = [ [] for _ in range(nTracks)]
 
-# creating tracklist
-for note in notes:
-    tracklist[note.track].append([note.octave(), note.pitch % 12,
-    note.onTimeMillis(tempoMap) / 1000, 0, note.noteEvent_on.rowNumber,
-    note.noteEvent_off.rowNumber ])
+    # creating tracklist
+    for note in notes:
+        tracklist[note.track].append([note.octave(), note.pitch % 12,
+        note.onTimeMillis(tempoMap) / 1000, 0, note.noteEvent_on.rowNumber,
+        note.noteEvent_off.rowNumber ])
 
-print(tracklist)
+    return tracklist
---
+++ rewrite_csv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+def rewrite_csv(csv_in, tracklist):
+    rows = open(csv_in, encoding="latin-1").read().splitlines()
+    for track in tracklist:
+        for measurement in track:
+            quantum_note = measurement[3]
+            note_on_line_number = measurement[4]
+            note_off_line_number = measurement[5]
+
+            for line_number in (note_off_line_number, note_on_line_number):
+                row = rows[line_number]
+                cells = row.split(", ")
+                cells[4] = quantum_note
+                rows[line_number] = ', '.join(cells)
+    return(rows)
